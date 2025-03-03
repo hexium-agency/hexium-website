00011a0706aa7be41891c6a888b1a32e2fd68438
Anthony <anthony@hexium.io>
2025-03-03 21:02:49 +0100

    chore: add default tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// Always prefer to use the global.css with Tailwind v4 new logic instead of this file
+// Use this config file only when needed
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	theme: {
+    extend: {}
+  }
+}
