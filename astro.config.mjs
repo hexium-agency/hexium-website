510f97c0238fca120344e464de2743a3db4abe98
Anthony <anthony@hexium.io>
2025-03-03 11:38:56 +0100

    chore: add react to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import { loadEnv } from 'vite';
 import { storyblok } from '@storyblok/astro';
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    react(),
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
       components: {
