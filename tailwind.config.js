f009dd181a5763cecf84ee08f33a1c6f3e13ae1b
Anthony <anthony@hexium.io>
2025-03-07 09:58:42 +0100

    chore: add tailwind typography specific configuration to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	theme: {
-    extend: {}
+    extend: {
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            'img': {
+              borderRadius: '.5rem',
+              marginTop: '2.5rem',
+              marginBottom: '2.5rem',
+              boxShadow: '0 0 0 1px rgba(19, 19, 22, .05)',
+              backgroundColor: 'rgba(94, 95, 110, .05)',
+            },
+            'h1 a, h2 a, h3 a, h4 a, h5 a, h6 a': {
+              color: 'inherit',
+              textDecoration: 'inherit',
+              fontWeight: 'inherit'
+            },
+            'ol > li::marker': {
+              fontWeight: '700',
+              fontFamily: 'var(--font-sans)',
+              color: 'var(--tw-prose-counters)',
+            },
+            '--tw-prose-counters': 'var(--color-gray-700)',
+            '--tw-prose-bullets': 'var(--color-gray-700)',
+            '--tw-prose-headings': 'var(--color-gray-950)',
+          },
+        },
+      }),
+    }
   }
 }
