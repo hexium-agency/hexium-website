db54b6dfe4718d1158de3ba5ae9bebf2d394acf3
Anthony <anthony@hexium.io>
2025-03-30 11:37:17 +0200

    feat: add logic to handle paginated blog home and categories

--- src/services/blog.ts
+++ src/services/blog.ts
@@ -1,8 +1,21 @@
-import type { ArticleStoryblok, DefinitionStoryblok } from '@/types/storyblok';
+import type { ArticleStoryblok, BlogCategoryStoryblok } from '@/types/storyblok';
 import { useStoryblokApi, type ISbStoriesParams } from '@storyblok/astro';
-import { type ISbStoryData } from '@storyblok/astro';
+import { type ISbStoryData, type ISbStories } from '@storyblok/astro';
 
-async function getLatest(category: string) {
+const ARTICLES_PER_PAGE = 12;
+
+async function getCategories() {
+  const storyblokApi = useStoryblokApi();
+
+  const { data } = await storyblokApi.get('cdn/stories', {
+    content_type: 'blogCategory',
+    version: import.meta.env.STORYBLOK_ENV,
+  });
+
+  return data.stories as ISbStoryData<BlogCategoryStoryblok>[];
+}
+
+async function getLatest(category?: string) {
   const storyblokApi = useStoryblokApi();
 
   const params: ISbStoriesParams = {
@@ -25,8 +38,58 @@ async function getLatest(category: string) {
   return data.stories as ISbStoryData<ArticleStoryblok>[];
 }
 
+async function getPaginated({ category, page = 1 }: { category?: string; page?: number }) {
+  const storyblokApi = useStoryblokApi();
+
+  const params: ISbStoriesParams = {
+    version: import.meta.env.STORYBLOK_ENV,
+    content_type: 'article',
+    per_page: ARTICLES_PER_PAGE,
+    sort_by: 'published_at:desc',
+    page,
+  };
+
+  if (category) {
+    params.filter_query = {
+      categories: {
+        in: category,
+      },
+    };
+  }
+
+  const { data } = await storyblokApi.get(`cdn/stories`, params);
+
+  return data.stories as ISbStoryData<ArticleStoryblok>[];
+}
+
+async function getTotal(category?: string) {
+  const storyblokApi = useStoryblokApi();
+
+  const params: ISbStoriesParams = {
+    version: import.meta.env.STORYBLOK_ENV,
+    content_type: 'article',
+    per_page: ARTICLES_PER_PAGE,
+    sort_by: 'published_at:desc',
+  };
+
+  if (category) {
+    params.filter_query = {
+      categories: {
+        in: category,
+      },
+    };
+  }
+
+  const data = (await storyblokApi.get(`cdn/stories`, params)) as ISbStories;
+
+  return Math.ceil(data.total / data.perPage);
+}
+
 const blogService = {
+  getCategories,
   getLatest,
+  getPaginated,
+  getTotal,
 };
 
 export default blogService;
