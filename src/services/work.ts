d1419b1643e369a6d3bf870bca19b5310711996f
Anthony <anthony@hexium.io>
2025-04-05 15:15:08 +0200

    feat: add work service

--- src/services/work.ts
+++ src/services/work.ts
@@ -1,8 +1,25 @@
-import type { WorkCategoryStoryblok, WorkStoryblok } from '@/types/storyblok';
+import type {
+  ArticleStoryblok,
+  BlogCategoryStoryblok,
+  WorkCategoryStoryblok,
+  WorkStoryblok,
+} from '@/types/storyblok';
 import { useStoryblokApi, type ISbStoriesParams } from '@storyblok/astro';
 import { type ISbStoryData, type ISbStories } from '@storyblok/astro';
 
 const WORKS_PER_PAGE = 12;
+
+async function getAllWorks() {
+  const storyblokApi = useStoryblokApi();
+
+  const articles = await storyblokApi.getAll('cdn/stories', {
+    content_type: 'work',
+    version: import.meta.env.STORYBLOK_ENV,
+    resolve_relations: ['work.category', 'work.customer', 'work.services', 'work.technologies'],
+  });
+
+  return articles as ISbStoryData<WorkStoryblok>[];
+}
 
 async function getCategories() {
   const storyblokApi = useStoryblokApi();
@@ -26,6 +43,30 @@ async function getCategoryBySlug(slug: string) {
   return data.story as ISbStoryData<WorkCategoryStoryblok>;
 }
 
+async function getLatest(category?: string) {
+  const storyblokApi = useStoryblokApi();
+
+  const params: ISbStoriesParams = {
+    version: import.meta.env.STORYBLOK_ENV,
+    content_type: 'work',
+    per_page: 4,
+    resolve_relations: ['work.category', 'work.customer', 'work.services', 'work.technologies'],
+    sort_by: 'published_at:desc',
+  };
+
+  if (category) {
+    params.filter_query = {
+      category: {
+        in: category,
+      },
+    };
+  }
+
+  const { data } = await storyblokApi.get(`cdn/stories`, params);
+
+  return data.stories as ISbStoryData<WorkStoryblok>[];
+}
+
 async function getPaginated({ category, page = 1 }: { category?: string; page?: number }) {
   const storyblokApi = useStoryblokApi();
 
@@ -33,13 +74,14 @@ async function getPaginated({ category, page = 1 }: { category?: string; page?: 
     version: import.meta.env.STORYBLOK_ENV,
     content_type: 'work',
     per_page: WORKS_PER_PAGE,
+    resolve_relations: ['work.category', 'work.customer', 'work.services', 'work.technologies'],
     sort_by: 'published_at:desc',
     page,
   };
 
   if (category) {
     params.filter_query = {
-      categories: {
+      category: {
         in: category,
       },
     };
@@ -62,7 +104,7 @@ async function getTotal(category?: string) {
 
   if (category) {
     params.filter_query = {
-      categories: {
+      category: {
         in: category,
       },
     };
@@ -74,8 +116,10 @@ async function getTotal(category?: string) {
 }
 
 const workService = {
+  getAllWorks,
   getCategories,
   getCategoryBySlug,
+  getLatest,
   getPaginated,
   getTotal,
 };
